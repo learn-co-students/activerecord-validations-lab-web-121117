4cffb2141c063b01795c7f838c55b346636bbcc3
tanelam <tania.e.aparicio@gmail.com>
2018-01-15 19:53:15 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :non_clickbait, acceptance: true
+  CLICKBAIT = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+
+  def non_clickbait
+    if CLICKBAIT.none? { |bait| bait.match title }
+      "This is clickbait"
+    end
+  end
 end
